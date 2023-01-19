f54789c820946d01ffabff641994e320a83cf8c4
BharathKesineni <bharath.kesineni@brainvire.com>
2023-01-19 10:57:25 +0530

    complete code

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-uffhkjjlki
+const http = require("http");
+const {requestHandler, userText} = require("./rout");
+console.log(userText)
+const server = http.createServer(requestHandler);
+
+server.listen(3000);
